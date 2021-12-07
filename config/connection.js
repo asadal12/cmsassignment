6ceb6cc31eab3c3069e1854378ade97be8774e71
asadal12 <85455544+asadal12@users.noreply.github.com>
2021-12-08 01:43:14 +0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-        host: 'localhost',
+    sequelize = new Sequelize('sql6457160', 'sql6457160', 'yfVGt9fh9t', {
+        host: 'sql6.freesqldatabase.com',
         dialect: 'mysql',
         port: 3306
     });
